f1ea329954d8d9b699c6d34839fd58f71160ad24
Kirill Novgorodcev <novgorodtsev.kirill@220-volt.ru>
2017-06-28 13:00:32 +0300

    improved pod

--- lib/Mojolicious/Plugin/Template/Mustache.pm
+++ lib/Mojolicious/Plugin/Template/Mustache.pm
@@ -49,23 +49,47 @@ Mojolicious::Plugin::Template::Mustache - Mojolicious Plugin
   # Mojolicious::Lite
   plugin 'Template::Mustache';
 
+  get '/inline' => sub {
+  my $c = shift;
+  $c->render(
+      handler => 'mustache',
+      inline  => 'Inline hello, {{message}}!',
+      message => 'Mustache',
+  );
+};
+
 =head1 DESCRIPTION
 
 L<Mojolicious::Plugin::Template::Mustache> is a L<Mojolicious> plugin.
 
 =head1 METHODS
 
-L<Mojolicious::Plugin::Template::Mustache> inherits all methods from
-L<Mojolicious::Plugin> and implements the following new ones.
-
-=head2 register
-
-  $plugin->register(Mojolicious->new);
-
-Register plugin in L<Mojolicious> application.
+L<Mojolicious::Plugin::Template::Mustache> inherits all methods from L<Mojolicious::Plugin>.
 
 =head1 SEE ALSO
 
-L<Mojolicious>, L<Mojolicious::Guides>, L<http://mojolicious.org>.
+L<Mojolicious>, L<Mojolicious::Guides>, L<http://mojolicious.org>, L<http://mustache.github.io>, L<Template::Mustache>.
+
+=head2 AUTHOR
+
+Cyrill Novgorodcev E<lt>cynovg@cpan.orgE<gt>
+
+=head2 LICENSE
+
+                    Copyright 2017 Cyrill Novgorodcev.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
+
